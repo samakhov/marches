391b53a23c99a2a185c4b0c9a8ccefc7c97c2b44
Asbath Biyalou-Sama <asamabiyalou@yahoo.com>
2022-04-27 09:59:44 +0200

    first init

---
+++ src/main/java/tg/univkara/fast/marches/MarchesApplication.java
@@ -0,0 +1,13 @@
+package tg.univkara.fast.marches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tg/univkara/fast/marches/MarchesApplicationTests.java
@@ -0,0 +1,13 @@
+package tg.univkara.fast.marches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
