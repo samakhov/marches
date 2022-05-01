a6663d838489e45ec1760ea3a123aa05c46fe717
Asbath Biyalou-Sama <asamabiyalou@yahoo.com>
2022-05-01 17:15:25 +0200

    implement controller and fix repo

--- src/main/java/tg/univkara/fast/marches/controller/MarcheController.java
+++ src/main/java/tg/univkara/fast/marches/controller/MarcheController.java
@@ -1,5 +1,23 @@
 package tg.univkara.fast.marches.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tg.univkara.fast.marches.services.MarcheServices;
+import tg.univkara.fast.marches.services.dto.MarcheDto;
+
+@RestController
 public class MarcheController {
 
+	@Autowired
+	private MarcheServices marcheServices;
+	
+	@GetMapping("/marches/all")
+	public List<MarcheDto> getMarches() {
+		return marcheServices.getAllMarches();
+	}
+	
 }
